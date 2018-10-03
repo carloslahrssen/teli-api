375d97668ccc1ff984925b70339922a815996964
carloslahrssen <carloslahrssen@gmail.com>
2018-10-03 16:01:29 -0400

    not default

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 const sendSms = async (source, destination, message, apiOptions) => {
   const response = await axios.post(
-    `${apiOptions.teliUrl}/sms/send?token=${apiOptions.apiToken}&source=${source}&destination=${destination}&message=${message}`
+    `${apiOptions.teliUrl}/sms/send?token=${
+      apiOptions.apiToken
+    }&source=${source}&destination=${destination}&message=${message}`
   );
 
   return response;
 };
 
-export default { sendSms };
+export { sendSms };
